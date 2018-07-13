441c45e2cf60eca53e716291bbb761fcfa0d1f0e
Avishainu <avishainudelman@se.shenkar.ac.il>
2018-07-13 17:32:50 +0300

    commiting desktop

---
+++ Final-project---desktop/includes/appointmentScripts.js
@@ -0,0 +1,52 @@
+/*++++++++++++++++++++++++++++++++++++++ Appointment ++++++++++++++++++++++++++++++++++++++*/
+
+var fromApp = document.getElementById("formAppointment");
+var SA = document.getElementById("selectAppointment");
+var ES = document.getElementById("EditSuccessfully");
+var isHamburgerOpen=1;
+
+function changeToEditPage() {
+    SA.style.display = "none";
+    fromApp.style.display = "block";
+}
+
+function saveEditionChanges() {
+    fromApp.style.display = "none";
+    ES.style.display = "block";
+}
+
+function hamburger(){
+    var mainObject=document.getElementById("appMain");
+    var navObject=document.getElementById("appNav");
+    console.log(mainObject.style.position);
+    console.log(mainObject.style.width);
+
+    if (isHamburgerOpen==1)
+    {
+        navObject.style.width="0%";
+        mainObject.style.width="100%";
+        isHamburgerOpen=0;
+    }else {
+        navObject.style.width="20%";
+        mainObject.style.width="80%";
+        isHamburgerOpen=1;
+    }
+}
+
+$(function() {
+    $('[data-toggle="datepicker"]').datepicker({
+        autoHide: true,
+        zIndex: 2048,
+    });
+});
+
+
+document.getElementById("chooseAppointmentToEdit").onclick = function () {
+    changeToEditPage();
+};
+
+document.getElementById("saveEditChanges").onclick = function () {
+    saveEditionChanges();
+};
+
+
---
+++ Final-project---desktop/includes/scripts.js
@@ -0,0 +1,55 @@
+/*++++++++++++++++++++++++++++++++++++++ Registration ++++++++++++++++++++++++++++++++++++++*/
+
+function showpass(){
+    var x=document.getElementById("userpassword");
+    if (x.type=="password"){
+        x.type="text";
+    }else{
+        x.type=x.type="password"
+    }
+
+}
+
+/*++++++++++++++++++++++++++++++++++++++ Header menu Icon ++++++++++++++++++++++++++++++++++++++*/
+
+var isHamburgerOpen=1;
+
+function hamburger(){
+    var mainObject=document.getElementById("indexMain");
+    var navObject=document.getElementById("indexNav");
+    console.log(mainObject.style.position);
+    console.log(mainObject.style.width);
+
+    if (isHamburgerOpen==1)
+    {
+        navObject.style.width="0%";
+        mainObject.style.width="100%";
+        isHamburgerOpen=0;
+    }else {
+        navObject.style.width="20%";
+        mainObject.style.width="80%";
+        isHamburgerOpen=1;
+    }
+}
+
+/*++++++++++++++++++++++++++++++++++++++ Notifications pop-up ++++++++++++++++++++++++++++++++++++++*/
+
+function show_or_hide_notifications() {
+    var notes = document.getElementById("notifications");
+    if (notes.style.display != "block")
+        notes.style.display = "block";
+    else
+        {
+            notes.style.display = "none";
+            document.getElementById("numOfNotifications").style.display = "none";
+        }
+};
+
+document.getElementById("numOfNotifications").onclick = function () {
+    show_or_hide_notifications();
+};
+
+document.getElementById("menuIcon").onclick = function () {
+   hamburger();
+};
+
